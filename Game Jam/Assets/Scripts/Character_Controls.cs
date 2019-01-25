79c28068a5aad72e4238b5a9bc38cfb7b40c7f6f
Nikolaienko-Valerii <valeranikolaenko17@gmail.com>
2019-01-26 01:31:29 +0200

    Added second player
    Added key mappings for second player
    Added multiplayer functionality

---
+++ Game Jam/Assets/Scripts/Character_Controls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controls : MonoBehaviour
+{
+    [SerializeField] float MovementSpeed = 0.1f;
+    private string playerName;
+
+    void Start()
+    {
+        if (gameObject.name == "Player1")
+        {
+            playerName = "_P1";
+        }
+        else
+        {
+            playerName = "_P2";
+        }
+
+    }
+
+    void Update()
+    {
+        gameObject.transform.Translate(Input.GetAxis("DPad_Horizontal" + playerName) * MovementSpeed, 0, 0);
+    }
+}
