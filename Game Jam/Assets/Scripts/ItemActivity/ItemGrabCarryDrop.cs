4b8e3b1b4d85d7d29679f6fc011405cd8b6324e0
Alexander <kinonadzami98@gmail.com>
2019-01-25 23:21:03 +0200

    Item scr

---
+++ Game Jam/Assets/Scripts/ItemActivity/ItemGrabCarryDrop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGrab : MonoBehaviour
+{
+    public float distractX;
+
+
+    public void Grab(GameObject player)
+    {
+        if(transform.position.x <= player.transform.position.x + distractX && transform.position.x >= player.transform.position.x)
+        {
+
+        }
+    }
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
