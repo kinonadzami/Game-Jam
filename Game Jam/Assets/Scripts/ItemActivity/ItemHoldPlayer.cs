1c963aa3240490e1090ec0bf0d4b3bfbe52f28fc
Alexander <kinonadzami98@gmail.com>
2019-01-26 00:02:53 +0200

    Item scr complete

--- Game Jam/Assets/Scripts/ItemActivity/ItemHoldPlayer.cs
+++ Game Jam/Assets/Scripts/ItemActivity/ItemHoldPlayer.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class ItemHoldPlayer : MonoBehaviour
 {
-    public GameObject holdingItem;
+    public GameObject holdingItem = null;
+    public float fall = 0;
+
+    public void TakeItem(GameObject item)
+    {
+        if (holdingItem == null)
+        {
+            holdingItem = item;
+            item.transform.SetParent(transform);
+            item.transform.localPosition = new Vector3(0, 0, 0);
+        }
+    }
+
+    public void DropItem()
+    {
+        if (holdingItem != null)
+        {
+            holdingItem.transform.SetParent(transform.parent);
+            holdingItem.transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - fall, -1);
+            holdingItem = null;
+        }
+    }
 
 
-
-
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DropItem();
+        }
+    }
 }
