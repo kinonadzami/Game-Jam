4d87399152611285bff1153d9f22d1d32b75ecfe
Nikolaienko-Valerii <valeranikolaenko17@gmail.com>
2019-01-26 02:30:57 +0200

    Multiple Gamepad Support

---
+++ Game Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float MovementSpeed = 0.1f;
+    private string playerName;
+
+    void Start()
+    {      
+        if (gameObject.name == "Player1")
+        {
+            playerName = "_P1";
+        }
+        if (gameObject.name == "Player2")
+        {
+            playerName = "_P2";
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        gameObject.transform.Translate(Input.GetAxis("DPad_Horizontal" + playerName) * MovementSpeed, 0, 0);
+    }
+}
