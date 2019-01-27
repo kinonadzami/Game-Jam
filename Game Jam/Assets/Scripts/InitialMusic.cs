f5dc077f13a3e8386f85577ce1a29bb13119cf7e
Nikita Yarovoy <nikita_yarovoy@ukr.net>
2019-01-27 14:44:56 +0200

    not resolved

---
+++ Game Jam/Assets/Scripts/InitialMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitialMusic : MonoBehaviour
+{
+    public GameObject pausePanel;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SoundManager.PlayMusic("Swinging Pants");
+        //SoundManager.PlayMusic("Nordic Title");
+    }
+    void Start()
+    {
+        //SoundManager.PlayMusic("Swinging Pants");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
